f266e4f9492b3c511d6b8b35aeaec5a8d0254195
克比 <coby3@qi-cloud.com>
2019-03-07 17:43:29 +0800

    初步完成nginx网关

--- discreteness-gateway/conf/zookeeper-gateway.lua
+++ discreteness-gateway/conf/zookeeper-gateway.lua
@@ -1,2 +1,21 @@
-local api = require "zoo.api"
+local zoo = require "zoo"
 local cjson = require "cjson"
+local uri = ngx.var.uri;
+
+if uri == "/" then
+	uri = "/url/root"
+else
+	uri = "/url" .. uri
+end
+local childs, err = zoo.childrens(uri)
+--[[ngx.say(cjson.encode(childs and childs or { error = err }))]]
+if childs ~= nil then
+	address = ""
+	for k, v in pairs(childs) do
+		address = v
+	end
+	return ngx.exec("/proxy",{proxyhost=address,uri=ngx.var.uri})
+else
+	return ngx.exec("/proxy",{proxyhost=reply})
+end
+
