20c51f40a4655aa5dc692a3899c435659fee2f0f
克比 <coby3@qi-cloud.com>
2019-03-12 15:43:11 +0800

    删除无用文件

--- pepper-common/pepper-common-model/src/main/java/com/qicloud/model/constant/WxConstant.java
+++
@@ -1,8 +0,0 @@
-package com.pepper.model.constant;
-
-public class WxConstant {
-	
-	public final static String WEIXIN_APPID = "weixin_appid";
-	
-	public final static String WEIXIN_SECRET = "weixin_secret";
-}
